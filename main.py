ebbee7b6f92358941ce59a3449cca251b4a2002d
Thomas Gerardin <devcapsys@capsys.eu>
2026-02-26 15:30:13 +0100

    Updates

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ class TestThread(QThread):
         self.skipped_steps = skipped_steps or set()
         self.steps = self.load_steps()
         self.generate_report = generate_report
+        self.runtime_logs = []
 
     def emit_log_message(self, message, color="white"):
         """Emit a log message signal with the given message and color."""
@@ -51,6 +52,9 @@ class TestThread(QThread):
                     message = json.dumps(obj, ensure_ascii=False, indent=2)
             except json.JSONDecodeError:
                 pass
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.runtime_logs.append(f"[{timestamp}] {message}")
         self.log_message.emit(message, color)
 
     def emit_step_percentage(self, step_idx, percentage):
@@ -223,7 +227,7 @@ class TestThread(QThread):
         if self.generate_report:
             try:
                 report = DeviceReport(config.db, int(device_id), debug=config.arg.show_all_logs)  # type: ignore[attr-defined]
-                report.fetch_data()
+                report.fetch_data(log_content="\n".join(self.runtime_logs))
                 report.generate_pdf_report(output_path)
                 if configuration.VERSION != "DEBUG":
                     os.startfile(output_path)
@@ -1031,11 +1035,11 @@ def main():
     if len(sys.argv) < 12:
         print("Aucun argument fourni, utilisation des paramètres par défaut pour le débogage.")
         # config.arg.operator = "Thomas GERARDIN"
-        # config.arg.commande = "1"
-        # config.arg.of = "1"
-        # config.arg.article = "radar"
-        # config.arg.indice = "1"
-        config.arg.product_list_id = configuration.PRODUCT_LIST_ID_DEFAULT
+        # config.arg.commande = "26300"
+        # config.arg.of = "45610"
+        # config.arg.article = "Template"
+        # config.arg.indice = "A"
+        # config.arg.product_list_id = configuration.PRODUCT_LIST_ID_DEFAULT
         # config.arg.user = "root"
         # config.arg.password = "root"
         # config.arg.host = "127.0.0.1"
@@ -1048,7 +1052,7 @@ def main():
         config.arg.of = sys.argv[3]
         config.arg.article = sys.argv[4]
         config.arg.indice = sys.argv[5]
-        config.arg.product_list_id = sys.argv[6]
+        # config.arg.product_list_id = sys.argv[6]
         config.arg.user = sys.argv[7]
         config.arg.password = sys.argv[8]
         config.arg.host = sys.argv[9]
